e92586a5a00dcac833f3e11e7c7525223396470e
CQuann <64736557+CQuann@users.noreply.github.com>
2024-01-31 14:10:16 +0300

    init project

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+var a = Math.round(Math.random()*3)
+image = [];
+image[0] = "./images/img1.png"
+image[1] = "./images/img2.png"
+image[2] = "./images/img3.png"
+image[3] = "./images/img0.png"
+document.write ("<img src=" + image[a] + ">");
